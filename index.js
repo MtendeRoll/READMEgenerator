db5f22c274e6a15213fe13bf0a36cb7e7d22f598
MtendeRoll <78053516+MtendeRoll@users.noreply.github.com>
2022-06-20 16:27:26 -0400

    added array of questions

--- index.js
+++ index.js
@@ -1,29 +1,41 @@
-// TODO: Include packages needed for this application
-// var inquirer = require("inquirer");
-import inquirer from "inquirer";
+//Packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
 
-// TODO: Create an array of questions for user input
-inquirer.prompt([
+// An array of questions for user input
+const questions = [
   {
     type: "input",
-    name: "projectTitle",
-    message: "What is the title of your project",
+    name: "credits",
+    message: "Please enter your GitHub username ",
   },
   {
     type: "input",
-    name: "description",
-    message: "Provide a short description of your project",
+    name: "github",
+    message: "Please enter your GitHub url",
   },
-  //Installation (installation instructions)
-  // Usage (usage information)
-  //Contributing (contribution guidelines)
-  //Tests (test instructions)
-  //choose a license
-  //License (add a notice explaining which license is chosen)
-  //Questions (enter email address, github username)
-  //clickable links to corresponding sections of README
-]);
-const questions = [];
+  { type: "input", name: "title", message: "What is the title of your project?" },
+  { type: "input", name: "description", message: "Please enter a description of your project." },
+  { type: "input", name: "installation", message: "Please describe the steps required to install your project." },
+  { type: "input", name: "usage", message: "Please provide instruction and examples for use of your project." },
+  {
+    type: "list",
+    name: "license",
+    message: "Which license are you using for your project?",
+    choices: [
+      { name: "MIT", value: "MIT" },
+      { name: "ISC", value: "ISC" },
+      { name: "APACHE 2.0", value: "APACHE" },
+      { name: "BSD", value: "BSD" },
+      { name: "GPL 3.0", value: "GPL" },
+    ],
+  },
+  { type: "input", name: "contributing", message: "Please enter information for how other developers can contribute." },
+  { type: "input", name: "tests", message: "Please provide examples of how to run your project tests." },
+  { type: "input", name: "email", message: "Please enter your email address." },
+  { type: "input", name: "repo", message: "Please enter the name of your github repository for this project." },
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
